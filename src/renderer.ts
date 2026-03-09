c8b0ffc0cf1a02b82a1c8362cab63e8b67310b8b
SergioMir018 <sergiommirs@gmail.com>
2026-03-09 00:35:27 -0400

    feat: added a basic scene with a sphere

--- src/renderer.ts
+++ src/renderer.ts
@@ -27,7 +27,67 @@
  */
 
 import './index.css';
+import * as THREE from 'three';
 
-console.log(
-  '👋 This message is being logged by "renderer.ts", included via Vite',
-);
+function main() {
+  const canvas = document.querySelector('#c');
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+
+  // camera
+  const fov = 75;
+  const aspect = 2;
+  const near = 0.1;
+  const far = 10;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 2;
+
+  // light
+  const color = 0xFFFFFF;
+  const intesity = 3;
+  const light = new THREE.DirectionalLight(color, intesity);
+  light.position.set(-1, 2, 4);
+
+  const scene = new THREE.Scene();
+
+  const geometry = new THREE.SphereGeometry(0.6, 35, 35);
+  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
+
+  const planet = new THREE.Mesh(geometry, material);
+
+  scene.add(planet);
+  scene.add(light);
+
+  function resizeRendererToDisplaySize(renderer: THREE.WebGLRenderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+
+    if(needResize) {
+      renderer.setSize(width, height, false);
+    }
+
+    return needResize;
+  }
+  
+  function render(time: number) {
+    time *= 0.001;
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    
+    planet.rotation.x = time;
+    planet.rotation.y = time;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+main();
